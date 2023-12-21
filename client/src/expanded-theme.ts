bd8eed4e0155b2e233d4cda3fcd2a5d858d1d8c2
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2023-12-21 14:35:13 +0530

    fixing expanded theme

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//@ts-ignore
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
